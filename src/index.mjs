7012f404b57b3178ad2eec7c437ec435550e28fd
橋本直美 <74492691+naomi-703@users.noreply.github.com>
2023-10-29 14:20:47 +0000

    Update index.html and index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -1,2 +1,43 @@
 import "./styles.css";
 
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // liタグ生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  // button(完了)タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  })
+;
+  // button(削除)タグ生成
+  const deletteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("完了");
+  }
+  
+  // divタグの子要素に各要素を設定
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+
+}
+
+document
+.getElementById("add-button")
+.addEventListener("click", () => onClickAdd());
